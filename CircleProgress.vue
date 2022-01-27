977f2b157f1af39eaa208b49ac58263001a42079
yazan-alnughnugh <yazan-n100@hotmail.com>
2022-01-28 01:34:23 +0300

    fix circle load

--- CircleProgress.vue
+++ CircleProgress.vue
@@ -1,10 +1,8 @@
 <template>
-    <div>
     <svg width="30" height="30" viewBox="0 0 120 120">
         <circle cx="60" cy="60" r="54" fill="none" stroke="#e6e6e6" stroke-width="12" />
         <circle :style="{'stroke-dashoffset': `calc(100 - ${percent})`}" class="percent fifty" cx="60" cy="60" r="54" fill="none" stroke="#3b82f6" stroke-width="12" pathLength="100" />
     </svg>
-    </div>
 </template>
 
 <script>
