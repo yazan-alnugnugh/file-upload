8df4ab16cd0c9fdb20b4c45d2377d89446fd5ecb
yazan-alnughnugh <yazan-n100@hotmail.com>
2022-01-28 01:35:32 +0300

    fix circle load

--- Index.vue
+++ Index.vue
@@ -16,7 +16,7 @@
 
             <div @drop.prevent="drop($event)" @click.self="clickOnInput()" @dragover.prevent="dragover($event)" class="cursor-pointer hover:bg-gray-50 mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                 <div @click.self="clickOnInput()" class="space-y-1 text-center">
-                    <svg  class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                    <svg  class=" mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                         <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                     <div class="flex text-sm text-gray-600">
